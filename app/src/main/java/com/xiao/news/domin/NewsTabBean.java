823b102522db621a6143b2a0fa94b550593eecbd
maxiaochun <490178341@qq.com>
2016-06-19 23:48:45 +0800

    6/19

---
+++ app/src/main/java/com/xiao/news/domin/NewsTabBean.java
@@ -0,0 +1,9 @@
+package com.xiao.news.domin;
+
+/**
+ * Created by hasee on 2016/6/19.
+ */
+public class NewsTabBean {
+
+
+}
---
+++ app/src/main/java/com/xiao/news/pager/TabDetailPager.java
@@ -0,0 +1,46 @@
+/*
+package com.xiao.news.pager;
+
+import android.app.Activity;
+import android.view.View;
+import android.widget.TextView;
+
+import com.xiao.news.R;
+import com.xiao.news.domin.NewsTabMenu;
+
+*/
+/**
+ * 页签页面对象
+ * Created by hasee on 2016/6/19.
+ *//*
+
+public class TabDetailPager {
+
+    public Activity mActivity;
+    public View mRootView;
+    private TextView mTabName;
+    private final NewsTabMenu.ShowapiResBodyBean.ChannelListBean mTabData;
+
+    public TabDetailPager(Activity activity, NewsTabd){
+        mActivity = activity;
+        mRootView = initView();
+        //页签数据
+        mTabData = newsTab;
+
+        initView();
+
+   }
+
+    public View initView() {
+        View view = View.inflate(mActivity, R.layout.pager_tab_detail, null);
+        mTabName = (TextView) view.findViewById(R.id.tv_tab_name);
+        mTabName.setText(mTabData.name);
+        return view;
+    }
+
+    public void initData(){
+
+
+    }
+}
+*/
